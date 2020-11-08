cf2e4d9224b0582d2e3c2128dea2371ab10b6241
bbelli <bbelli@purdue.edu>
2020-11-08 15:57:15 -0500

    modified:   hw10.c 	modified:   main.c

--- HW10Cake2/hw10.c
+++ HW10Cake2/hw10.c
@@ -55,33 +55,30 @@ ListNode * createList(int valn){
 void eliminate(ListNode * head, int valk){
   ListNode * p = head;
   int count = 1;
-  while(p != NULL){
-    if((count) == valk){ 
+  while(head != NULL){
+     if(count == valk){
         count = 1;
         #ifdef DEBUG
-        // this #ifdef ... #endif should be inside the condition *BEFORE* a
-        // node' value is printed and it is deleted
-        ListNode * todelete = p; 
-        printListNode (todelete); 
+        ListNode * todelete = p;
+        printListNode(todelete);
         #endif
-        //ListNode * check = p;
-        p = deleteNode(head, p);
-    } //Else statement?
-    count++;
-    if((p->next) == NULL){
-      p = head;
-    }
-    else{
-      p = p->next;
-    }
+        p = deleteNode(head, todelete);
+     }
+     if((p->next) == NULL){
+          p = head;
+      }
+      else{
+          p = p->next; 
+      }
+       count++;
   }
+}
 // #ifdef DEBUG
 //   // this #ifdef ... #endif should be inside the condition *BEFORE* a
 //   // node' value is printed and it is deleted
 //   ListNode * todelete = p; //Does this take care of assigning the value? No need to do anything in/after while loop?
 //   printListNode (todelete); 
 // #endif
-}
 #endif
 
 #ifdef TEST_DELETENODE
@@ -100,34 +97,103 @@ void eliminate(ListNode * head, int valk){
 // It is possible that todelete is the first node in the list (i.e.,
 // the head). If this occurs, return the second node of the list.
 ListNode * deleteNode(ListNode * head, ListNode * todelete){
+  ListNode * p = head;
+  ListNode * q = p->next; //to be deleted
+  ListNode * returnNode = todelete->next;
   //1. If head is NULL, the list is empty and this function returns NULL
   if(head == NULL){
-    return NULL;
+    return returnNode;
   }
   //2. If todelete is NULL, nothing can be deleted, return head
   if(todelete == NULL){
-    return head;
+    return returnNode;
   }
   //Delete
-  //If todelete node is the first node
-  ListNode * p = head;
-  ListNode * q = p->next; //to be deleted
+
   if(head == todelete){
-    p = head-> next; //Is this correct?
     free(head);
-    return p; 
+    return returnNode; 
   }
   //Find todelete value in the Linked List and delete
   while((q!= NULL) && ((q->value) != (todelete->value))){
     p = p->next;
     q = q->next;
   }
-  if(q != NULL){ //3. If todelete is not in the list, keep the list unchanged and return head
+  if(q != NULL){ //3. If todelete is  in the list, delete.
     p->next = q->next;
     free(q);
   }
-  return head;
+  return returnNode;
 }
 #endif
 
-
+// void eliminate(ListNode * head, int valk){
+//   ListNode * p = head;
+//   int count = 1;
+//   while(head->next != NULL){
+//     count = valk;
+//     while((count) > 0 ){ 
+//         if(p != NULL){
+//           count--;
+//         }
+//         #ifdef DEBUG
+//         // this #ifdef ... #endif should be inside the condition *BEFORE* a
+//         // node' value is printed and it is deleted
+//         ListNode * todelete = p; 
+//         printListNode (todelete); 
+//         #endif
+//         ListNode * newHead = deleteNode(head, p);
+//         ListNode * check = p->next;
+//         if(check == NULL){
+//           p = newHead;
+//         }
+//         else{
+//           p = check;
+//         }
+//     } /
+//   }
+// #ifdef TEST_DELETENODE
+// // head points to the first node in the linked list
+// // todelete points  to the node to be deleted
+// //
+// // delete the node and return the head of the linked list
+// // release the memory of the deleted node
+// //
+// // should check several conditions:
+// // 1. If head is NULL, the list is empty and this function returns
+// //    NULL
+// // 2. If todelete is NULL, nothing can be deleted, return head
+// // 3. If todelete is not in the list, keep the list unchanged and
+// //    return head
+// // It is possible that todelete is the first node in the list (i.e.,
+// // the head). If this occurs, return the second node of the list.
+// ListNode * deleteNode(ListNode * head, ListNode * todelete){
+//   //1. If head is NULL, the list is empty and this function returns NULL
+//   if(head == NULL){
+//     return NULL;
+//   }
+//   //2. If todelete is NULL, nothing can be deleted, return head
+//   if(todelete == NULL){
+//     return head;
+//   }
+//   //Delete
+//   //If todelete node is the first node
+//   ListNode * p = head;
+//   ListNode * q = p->next; //to be deleted
+//   if(head == todelete){
+//     p = head-> next; //Is this correct?
+//     free(head);
+//     return p; 
+//   }
+//   //Find todelete value in the Linked List and delete
+//   while((q!= NULL) && ((q->value) != (todelete->value))){
+//     p = p->next;
+//     q = q->next;
+//   }
+//   if(q != NULL){ //3. If todelete is  in the list, delete.
+//     p->next = q->next;
+//     free(q);
+//   }
+//   return head;
+// }
+// #endif
--- HW10Cake2/main.c
+++ HW10Cake2/main.c
@@ -21,6 +21,7 @@ int main(int argc, char * * argv)
     }
   ListNode * head = NULL;
   head = createList(valn);
+  //printListNode(head);
   eliminate(head, valk);
   return EXIT_SUCCESS;
 }

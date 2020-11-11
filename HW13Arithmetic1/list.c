6079a6fda2ebc90bbf1c507cf798db4ea1a98031
Berkay Belli <bbelli@purdue.edu>
2020-11-11 10:18:49 -0500

    modified:   list.c

--- HW13Arithmetic1/list.c
+++ HW13Arithmetic1/list.c
@@ -21,8 +21,26 @@
 //    fclose
 //    arithlist points to the head and tail of the list
 //    return true
-bool readList(char * filename, List * arithlist)
-{
+bool readList(char * filename, List * arithlist){
+  // If arithlist is NULL, return false
+  if(arithlist == NULL){
+    return false;
+  }
+  FILE *fptr = fopen(filename, "r");
+  // If fopen fails, return false
+  if(fptr == NULL){
+    return false;
+  }
+  char inputs[WORDLENGTH];
+  while(fgets(inputs, WORDLENGTH, fptr) != NULL){
+    if(strchr(inputs, '\n') == NULL){
+      deleteList(arithlist); //    free memory of the list
+      fclose(fptr); //    fclose
+      return false;
+    }
+    addNode(arithlist, inputs);
+  }
+  fclose(fptr);
   return true;
 }
 #endif
@@ -31,8 +49,19 @@ bool readList(char * filename, List * arithlist)
 // If arithlist is NULL, do nothing
 // release the memory of every node in the list
 // release the memory of the list 
-void deleteList(List * arithlist)
-{
+void deleteList(List * arithlist){
+  
+  if(arithlist == NULL){
+    return;
+  }
+  ListNode *a = arithlist->head;
+
+  while(a){
+    ListNode *temp = a->next;
+    free(a); // release the memory of every node in the list
+    a = temp;
+  }
+    free(arithlist); // release the memory of the list 
 }
 #endif
 
@@ -48,8 +77,20 @@ void deleteList(List * arithlist)
 // allocate memory for a new ListNode
 // copy word to the word attribute of the new ListNode
 // insert the ListNode to the list
-void addNode(List * arithlist, char * word)
-{
+void addNode(List * arithlist, char * word){
+  // If arithlist is NULL, do nothing
+  if(arithlist == NULL){
+    return;
+  }
+  // allocate memory for a new ListNode
+  ListNode * newNode = malloc(sizeof(ListNode));
+  strcpy(newNode->word, word); //Copy word given by function argument into newNode.
+  newNode->prev = NULL;
+  newNode->next = NULL;
+  ListNode * currentTail = arithlist->tail; //store current tail
+  arithlist->tail->next = newNode; //set newNode as new tail (add newNode to the very end)
+  newNode->prev = currentTail; //set newNode as new tail
+  newNode->next = NULL; //set tail->next = NULL;
 }
 #endif
 
@@ -67,8 +108,55 @@ void addNode(List * arithlist, char * word)
 // return true.
 //
 // Be careful about delete the first or the last node
-bool deleteNode(List * arithlist, ListNode * ln)
-{
+bool deleteNode(List * arithlist, ListNode * ln){
+  // If arithlist is NULL, return false
+  // If the list is empty (head and tail are NULL), return false
+  if((arithlist == NULL) || ((arithlist->head == NULL) && (arithlist->tail == NULL))){
+    return false;
+  }
+// If ln is not in the list, return false
+//Search for ln in while loop. 
+  ListNode * a = arithlist->head;
+  ListNode *temp = a->next;
+  while(a){
+    if(temp == ln){
+      break;
+    }
+  }
+  //If temp cannot find ln in while loop, return false. 
+  // If ln is not in the list, return false
+  if(temp != ln){
+    return false;
+  }
+  //It is possible that ln is the first node in the list (i.e.,
+  // the head). If this occurs, return the second node of the list.
+  if((arithlist->head) == ln){
+    ListNode * secondNode = arithlist->head->next;
+    arithlist->head = secondNode;
+    free(arithlist->head);
+    return true;
+  }
+   //It is possible that ln is the tail node in the list (i.e.,
+  // the tail). If this occurs, return the second node from the tail of the list.
+    if((arithlist->tail) == ln){
+    ListNode * secondNode = arithlist->tail->prev;
+    free(arithlist->tail);
+    arithlist->tail = secondNode;
+    return true;
+  }
+
+  //Delete
+  //Find todelete value in the Linked List and delete
+  ListNode * p = arithlist->head;
+  ListNode * q = p->next; //to be deleted
+  // while((q!= NULL) && ((q->value) != (ln->value))){ //WON'T WORK CORRECTLY
+  //   p = p->next;
+  //   q = q->next;
+  // }
+  if(q != NULL){ //3. If todelete is  in the list, delete.
+    p->next = q->next;
+    free(q);
+  }
   return true;
 }
 #endif
